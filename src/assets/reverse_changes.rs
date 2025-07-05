db756ce74e84b8af43fc8b4b2525fdfee580c7d0
PonasKovas <mykolas.peteraitis@gmail.com>
2025-07-05 21:48:42 +0300

    add functionality to detect rotmg update when rotmguard is running

--- src/assets/reverse_changes.rs
+++ src/assets/reverse_changes.rs
@@ -1,15 +1,18 @@
 use std::{
-	io::{self, Error},
+	fs::File,
+	io::{self, Error, Read},
 	path::{Path, PathBuf},
 };
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
 /// This cleans up and reverses the changes to resources.assets file on drop
 pub struct ReverseChangesGuard {
 	// the path to the renamed original assets
-	real_assets_path: PathBuf,
+	backup_assets_path: PathBuf,
 	// the path to the edited assets
 	edited_assets_path: PathBuf,
+	// BLAKE3 hash of the modified resources.assets file
+	hash: blake3::Hash,
 }
 
 impl ReverseChangesGuard {
@@ -20,8 +23,9 @@ impl ReverseChangesGuard {
 		std::fs::rename(assets_path, &new_path)?;
 
 		let guard = ReverseChangesGuard {
-			real_assets_path: Path::new(&new_path).to_path_buf(),
+			backup_assets_path: Path::new(&new_path).to_path_buf(),
 			edited_assets_path: assets_path.to_owned(),
+			hash: blake3::hash(contents),
 		};
 
 		std::fs::write(assets_path, contents)?;
@@ -48,18 +52,53 @@ impl ReverseChangesGuard {
 
 		Ok(guard)
 	}
+
+	fn finish(&mut self) -> io::Result<()> {
+		// normally we want to delete the edited assets and rename the original back to its place
+		// but if the edited assets have been changed on the disk since we wrote them
+		// its likely that the user updated rotmg and overwrote it
+		//
+		// in that case we dont want to do anything, just delete the backup if its still there
+		let mut file = File::open(&self.edited_assets_path)?;
+
+		let mut hasher = blake3::Hasher::new();
+		let mut buf = [0u8; 64 * 1024];
+		loop {
+			let n_read = file.read(&mut buf)?;
+			if n_read == 0 {
+				break;
+			}
+			hasher.update(&buf[..n_read]);
+		}
+
+		let hash = hasher.finalize();
+
+		if self.hash != hash {
+			// uh oh... file changed
+			warn!(
+				"resources.assets changed on disk while rotmguard was running. Leaving it as it is."
+			);
+			std::fs::remove_file(&self.backup_assets_path)?;
+		} else {
+			// all fine, standard procedure
+			std::fs::remove_file(&self.edited_assets_path)?;
+			std::fs::rename(&self.backup_assets_path, &self.edited_assets_path)?;
+
+			info!("Successfully reversed changes to game files.");
+		}
+
+		Ok(())
+	}
 }
 
 impl Drop for ReverseChangesGuard {
 	fn drop(&mut self) {
-		// delete the edited assets and rename original back to its place
-		if let Err(e) = std::fs::remove_file(&self.edited_assets_path)
-			.and_then(|_| std::fs::rename(&self.real_assets_path, &self.edited_assets_path))
-		{
+		if let Err(e) = self.finish() {
 			error!("Error reversing changes to game files: {e:?}");
-			error!("To do it manually: delete the `resources.assets` file, and rename `resources.assets.rotgmuard` to `resources.assets`.")
-		} else {
-			info!("Successfully reversed changes to game files.");
+			error!(
+				"To do it manually: delete the `resources.assets` file, and rename `resources.assets.rotgmuard` to `resources.assets`."
+			);
+			error!("Or just clear all game data and reinstall.");
 		}
 	}
 }
